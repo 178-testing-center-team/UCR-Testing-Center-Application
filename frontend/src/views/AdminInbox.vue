6d60fcc6acacf6cde8809717fbf7839207863884
katdario <62965281+katdario@users.noreply.github.com>
2022-11-28 14:00:22 -0800

    created admin inbox nav bar

--- frontend/src/views/AdminInbox.vue
+++ frontend/src/views/AdminInbox.vue
@@ -10,5 +10,14 @@ export default {
 </script>
 
 <template>
-    <Header title="Admin Inbox"/>
+    <div class="admin-inbox">
+        <Header title="Admin Inbox"/>
+        <NavBar :nav-elements="[
+            { title: 'Dashboard', img: 'dashBoard-icon.svg', route: '/admin' },
+            { title: 'Rooms', img: 'roomNav-icon.svg', route: 'admin/rooms' },
+            { title: 'Inbox', img: 'mailNav-icon.svg', route: 'admin/inbox' },
+            { title: 'Settings', img: 'settings-icon.svg', route: 'admin/settings' },
+            { title: 'My Account', img: 'profile-icon.svg', route: 'admin/account' },
+            { title: 'Logout', img: 'logout-icon.svg', route: '/login' }]" />
+    </div>
 </template>
