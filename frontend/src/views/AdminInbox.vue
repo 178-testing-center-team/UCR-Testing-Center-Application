a7bbfabc2b355a91917573c96cf8f273c31f0dad
katdario <62965281+katdario@users.noreply.github.com>
2022-11-28 11:04:18 -0800

    created admin inbox header using existing header component

--- frontend/src/views/AdminInbox.vue
+++ frontend/src/views/AdminInbox.vue
@@ -1,3 +1,14 @@
+<script>
+import Card from "../components/Card.vue"
+import NavBar from "../components/NavBar.vue"
+import Header from "../components/Header.vue"
+export default {
+    components: {
+        Card, NavBar, Header
+    }
+}
+</script>
+
 <template>
-    <h1>Admin Inbox</h1>
+    <Header title="Admin Inbox"/>
 </template>
