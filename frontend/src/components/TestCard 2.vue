a8af7d8f947012c7d519761b5f8d2aed9496d55f
katdario <62965281+katdario@users.noreply.github.com>
2023-02-23 11:16:30 -0800

    change admin-home name to page-contents

---
+++ frontend/src/components/TestCard 2.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    props: {
+        buttonHeader: String,
+        imgName: String,
+        route: String
+    }
+}   
+</script>
+
+<template>
+    <div class="test-card">
+        <router-link :to="route">
+            <button class="test-card-btn">{{ buttonHeader }}</button>
+        </router-link>
+    </div>
+</template>
